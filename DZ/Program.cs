dc1818e0d4dfde33167606d06bcff6f2a34cb3d6
Anastasia Mazina <MazinaAnastasiaC@gmail.com>
2024-03-19 17:34:04 +0000

    add DZ

---
+++ DZ/Program.cs
@@ -0,0 +1,77 @@
+﻿
+// int firstNumber = 6;
+// int secondNumber = 7;
+
+// // Введите свое решение ниже
+// if (firstNumber == secondNumber)
+// {
+//     Console.WriteLine("числа равны");
+// }
+// else
+// {
+//     if (firstNumber > secondNumber)
+//     {
+//         Console.WriteLine("первое число больше");
+//     }
+//     else
+//     {
+//         Console.WriteLine("второе число больше");
+//     }
+// }
+
+////2
+
+// int a = -3;
+// int b = 9;
+// int c = 9;
+
+// int max = a;
+
+// if (b > max)
+// {
+//     max = b;
+// }
+// if (c > max)
+// {
+//     max = c;
+// }
+// Console.WriteLine($"{max}");
+
+// //3
+
+// int number = -9;
+
+// if (number % 2 == 0)
+// {
+//     Console.WriteLine("четное");
+// }
+// else 
+// {
+//     Console.WriteLine("нечетное");
+// }
+
+
+// //4.
+
+// int number = 8;
+// int i = 1;
+
+// while (i <= number)
+// {
+//     if (i % 2 == 0)
+//     {
+//         Console.Write($"{i} ");
+//         i++;
+//     }
+//     else 
+//     {
+//         i++;
+//     }
+// }
+
+//// или
+
+// for (int count = 2; count <= number; count += 2)
+//         {
+//             Console.Write($"{count} ");
+//         }
