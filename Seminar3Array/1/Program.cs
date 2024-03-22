318e8a20c84fbc4b0995a1db691b8c7a1189e349
Anastasia Mazina <MazinaAnastasiaC@gmail.com>
2024-03-22 22:06:51 +0000

    add Seminar3Array

---
+++ Seminar3Array/1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Присутсвует ли заданное число в массиве
+
+
+Console.Write("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+
+// Console.WriteLine(array);
+
+
+// while
+int ind = 0;
+while (ind < array.Length)
+{
+    Console.Write($"{array[ind]} ");
+    ind++;
+}
+Console.WriteLine();
+
+
+// for
+
+for (int i = 0; i < array.Length; i++)
+{
+    //array[i] = 7;
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+// foreach
+
+foreach (var item in array) // var - автоматически пределяет тип переменной, можно проставить свой
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine();
+
+// функция
+
