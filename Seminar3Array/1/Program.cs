4447db37a2bbe980285eb6a070466347bdc6a691
Anastasia Mazina <MazinaAnastasiaC@gmail.com>
2024-03-22 22:57:08 +0000

    add info S3:1

--- Seminar3Array/1/Program.cs
+++ Seminar3Array/1/Program.cs
@@ -38,3 +38,48 @@ Console.WriteLine();
 
 // функция
 
+void PrintMas(int[] array)  // тип void означает функцию которая ничего не выводит 
+{
+    foreach (var item in array) // var - автоматически пределяет тип переменной, можно проставить свой
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+PrintMas(array);
+
+
+int[] RandomMas(int[] col) // функция - возвращает целочисленное значение int
+{
+    for (int i = 0; i < col.Length; i++) //проходимся по массиву и каждому элементу с i индексом приваиваем рндомное значение
+    {
+        col[i] = new Random().Next(1,9);
+    }
+
+    return col;
+}
+
+int[] arr = RandomMas(array);
+
+PrintMas(arr);
+
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int flag = 0;
+foreach (var item in arr)
+{
+    if (item == m)
+    {
+        flag++;
+    }
+}
+
+if (flag > 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
