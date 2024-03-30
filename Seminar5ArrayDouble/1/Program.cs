f9064c1451d35cca6309f2641d8add02d2f6c9bb
Anastasia Mazina <MazinaAnastasiaC@gmail.com>
2024-03-30 01:24:22 +0000

    add info S5

---
+++ Seminar5ArrayDouble/1/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные,
+//  и замените эти элементы на их квадраты.
+
+int[,] RandomMas(int[,] col) //функция 
+{
+    for (int i = 0; i < col.GetLength(0); i++)
+    {
+        for (int  j = 0; j < col.GetLength(1); j++)
+        {
+            col[i,j] = new Random().Next(0, 10);
+        }
+    }
+    return col;
+}
+
+
+void PrintMas(int[,] col)   
+{
+    for (int i = 0; i < col.GetLength(0); i++)
+    {
+        for (int  j = 0; j < col.GetLength(1); j++)
+        {
+            Console.Write($"{col[i,j]} "); 
+        }
+    Console.WriteLine(); 
+    }
+}
+
+int[,] Sq(int[,] col) //функция 
+{
+    for (int i = 0; i < col.GetLength(0); i++)
+    {
+        for (int  j = 0; j < col.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            {
+                col[i,j] = col[i,j] * col[i,j];
+            }
+           
+        }
+    }
+    return col;
+}
+
+
+Console.Write("Введите размер m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m,n];
+int[,] arrRandom = RandomMas(array);
+
+// int[,] arrRandom = {{2,3,4},{5,6,7}};
+
+PrintMas(arrRandom);
+Console.WriteLine(); 
+PrintMas(Sq(arrRandom));
---
+++ Seminar5ArrayDouble/2/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной 
+// диагонали (с индексами (0,0); (1;1) и т.д.
+
+
+int[,] RandomMas(int[,] col) //функция 
+{
+    for (int i = 0; i < col.GetLength(0); i++)
+    {
+        for (int  j = 0; j < col.GetLength(1); j++)
+        {
+            col[i,j] = new Random().Next(0, 10);
+        }
+    }
+    return col;
+}
+
+
+void PrintMas(int[,] col)   
+{
+    for (int i = 0; i < col.GetLength(0); i++)
+    {
+        for (int  j = 0; j < col.GetLength(1); j++)
+        {
+            Console.Write($"{col[i,j]} "); 
+        }
+    Console.WriteLine(); 
+    }
+}
+
+
+int Diag(int[,] col) //функция 
+{
+    int sum = 0;
+    for (int i = 0; i < col.GetLength(0); i++)
+    {
+        for (int  j = 0; j < col.GetLength(1); j++)
+        {
+            if(i == j)
+            {
+                sum = sum + col[i,j];
+            }
+        }
+    }
+    return sum;
+}
+
+// //или
+// int MinList(int[,] col) //функция определяющая что меньше количество строк или столбцов
+// {
+//     if (col.GetLength(0) < col.GetLength(1))
+//     {
+//         return col.GetLength(0);
+//     }
+//     return col.GetLength(1);
+// }
+
+// int DiagList(int[,] col, int min) //функция 
+// {
+//     int sum = 0;
+//     for (int i = 0; i < min; i++)
+//     {
+//        sum = sum + col[i,i];
+//     }
+//     return sum;
+// }
+// int res = DiagList(arrRandom, MinList(arrRandom));
+
+Console.Write("Введите размер m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m,n];
+int[,] arrRandom = RandomMas(array);
+
+PrintMas(arrRandom);
+Console.WriteLine(); 
+int res = Diag(arrRandom);
+Console.WriteLine($"Сумма равна {res}");
