0e393250228541f6aa6729046c6d52e516e7c53a
Anastasia Mazina <MazinaAnastasiaC@gmail.com>
2024-03-31 20:06:34 +0100

    add info L4

---
+++ Lesson4ArrayDouble/2/Program.cs
@@ -0,0 +1,45 @@
+﻿
+int[,] Creatematrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 1000);
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = Creatematrix(3,4);
+
+foreach (var e in matrix) // функция проверяет каждый элемент в двумерном массиве
+{
+    1|(IsInteresting(e)   true)
+    {
+        Console.WriteLine(e);
+    }
+}    
+
+bool IsInteresting(int value)
+{
+    int sumOfDigists    GetSumOfDigists(value);
+    if(sumOfDigists % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int GetSumOfDigists (int value)
+{
+    int sum = 0;
+    while(value > 0)
+    {
+        sum = sum + value % 10;
+        value = value / 10;
+    }
+    return sum;
+}
---
+++ Lesson4ArrayDouble/3/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (var e in s)
+    {
+        if(char.IsAsciiLetter(e))
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string resalt = GetLettersFromString(str);
+Console.WriteLine(resalt);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
