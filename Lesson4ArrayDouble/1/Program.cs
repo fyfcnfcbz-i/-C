6d9280a015bc5ebdbe83009766c38b41257f5ebd
Anastasia Mazina <MazinaAnastasiaC@gmail.com>
2024-03-31 18:49:08 +0100

    add L4:1

---
+++ Lesson4ArrayDouble/1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+int[,] Creatematrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShomMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix =Creatematrix(4,5);
+ShomMatrix(matrix);
