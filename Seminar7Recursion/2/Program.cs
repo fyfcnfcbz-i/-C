de2c087c93d0733a691d2fdb112daa116126c311
Anastasia Mazina <MazinaAnastasiaC@gmail.com>
2024-04-09 15:53:10 +0100

    add S7

---
+++ Seminar7Recursion/2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от 1 до N.
+
+
+string Len(int stop, int start = 1)
+{
+    if(start == stop) return Convert.ToString(stop);
+    return start + " " + Len(stop, start + 1);
+
+}
+
+void Len2(int num)
+{
+    if(num == 0) return;
+    //Console.Write($"{num} "); // печать ноборот от N до 1 (5 4 3 2 1)
+    Len2(num - 1);
+    Console.Write($"{num} "); // печать (1 2 3 4 5)
+}
+
+
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Len(n));
+
+Len2(n);
+
+
---
+++ Seminar7Recursion/3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Считать строку с консоли, содержащую латинские буквы. 
+// Вывести на экран согласные буквы этой строки
+
+void Consonants (string s)
+{
+    if(s.Length == 0) return;
+    string st = "aoyuei";
+    if (st.Contains(s[0]) == false) Console.Write($"{s[0]}");// функция Contains проверяет элемент  с индексом в строке st -> 1. если есть(истина) - лож -> Лож (не выводим на печать) / 2. если не(лож) - лож -> Истина (выводим на печать)
+    Consonants (s[1..]); // с 1 символа строки s
+}
+
+void Consonants2 (string s, int i = 0)
+{
+    string st = "aouei";
+    if(i == st.Length) return;
+    if (st.Contains(s[i]) == false) Console.Write($"{s[i]}");
+    Consonants2 (s, i + 1);
+}
+
+
+// // отрицание при помощи !
+// void Consonants2 (string s, int i = 0)
+// {
+//     string st = "aouei";
+//     if(i == st.Length) return;
+//     if (!st.Contains(s[i])) Console.Write($"{s[i]}");
+//     Consonants2 (s, i + 1);
+// }
+// Console.WriteLine(false); //вывод -> false
+// Console.WriteLine(!false); //вывод -> true
+
+
+
+string n = Console.ReadLine();
+Consonants(n);
+Console.WriteLine();
+Consonants2(n);
